6e4fe5992b3d6c5facb4b7e9761ae8d41cc7e987
Jonas Hahnfeld <hahnjo@hahnjo.de>
2019-10-04 18:13:07 +0200

    WIP for Python 3 on darwin-* and mingw
    For darwin, it's throwing errors in system headers while compiling
    Modules/posixmodule.c.
    mingw requires at least a toolchain update to provide `wcsnlen_s'
    and with a bit of patching, it's now compiling all source files and
    fails when linking. The reason seems to be that there are some files
    in python3-3.7.4/PC that provide symbols actually needed by Modules/.
    However, they are only referenced by the project files in PCbuild/,
    but not compiled by the provided Makefiles :-(

--- gub/specs/python3.py
+++ gub/specs/python3.py
@@ -24,10 +24,54 @@ PYTHON_FOR_BUILD=%(tools_prefix)s/bin/python3
 # shouldn't be too bad...
 
     def patch (self):
+        # Patch out error messages about untested cross build configurations.
+        self.file_sub ([
+            ('(AC_MSG_ERROR\(\[cross build not supported.*\]\))', '#\\1'),
+            ('(AC_MSG_ERROR\(\[readelf for the host is required for cross builds\]\))', '#\\1')
+        ], '%(srcdir)s/configure.ac')
         # Make setup.py work with tools::python3 as PYTHON_FOR_BUILD.
         self.file_sub ([('srcdir = sysconfig.*', 'srcdir = \'%(srcdir)s\'')],
                        '%(srcdir)s/setup.py')
         target.AutoBuild.patch (self)
+
+disable_implicit_function_declaration_error = '''
+ac_cv_enable_implicit_function_declaration_error=no
+'''
+
+class Python3__darwin (Python3):
+    config_cache_overrides = (Python3.config_cache_overrides
+        + disable_implicit_function_declaration_error)
+
+class Python3__mingw (Python3):
+    # Python/pylifecycle.c:460:5: error: implicit declaration of function 'setenv'
+    config_cache_overrides = (Python3.config_cache_overrides
+        + disable_implicit_function_declaration_error)
+    def patch (self):
+        # mingw only has the old definition of wcstok, replace by wcstok_s with
+        # the correct signature.
+        self.file_sub ([('wcstok', 'wcstok_s')], '%(srcdir)s/Python/pathconfig.c')
+        # Use implementations for Windows.
+        self.copy ('%(srcdir)s/PC/dl_nt.c', '%(srcdir)s/Python/dl_nt.c')
+        self.file_sub ([
+                ('Modules/getpath\\.c', 'PC/getpathp.c'),
+                ('(Modules/_io/_iomodule\\.o)', '\\1 Modules/_io/winconsoleio.o'),
+                ('(Python/importdl\\.o )', '\\1Python/dynload_win.o Python/dl_nt.o'),
+            ], '%(srcdir)s/Makefile.pre.in')
+        # Don't build some modules.
+        self.file_sub ([
+                ('^(pwd )', '# \\1'),
+                ('(_io/_iomodule\\.c)', '\\1 _io/winconsoleio.c'),
+            ], '%(srcdir)s/Modules/Setup.dist')
+        # Fix build of posixmodule.c on Windows.
+        self.file_sub ([
+                ('_MSC_VER', 'MS_WINDOWS'),
+                ('(#include "winreparse.h")', '\\1\n#define ALL_PROCESSOR_GROUPS 0xffff'),
+            ], '%(srcdir)s/Modules/posixmodule.c')
+        Python3.patch (self)
+    def configure (self):
+        Python3.configure (self)
+        self.copy ('%(srcdir)s/PC/pyconfig.h', '%(builddir)s/pyconfig.h')
+        self.copy ('%(srcdir)s/PC/errmap.h', '%(builddir)s/errmap.h')
 
 class Python3__tools (tools.AutoBuild, Python3):
     dependencies = ['autoconf', 'automake', 'libtool']
